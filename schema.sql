0703261af72828078f291eb5317c0ca03907783e
jithu-keyvalue <jithu@keyvalue.systems>
2025-04-10 11:13:40 +0530

    update

--- schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS observations (
-    id SERIAL PRIMARY KEY,
-    date DATE NOT NULL,
-    let-us-keep-a-better-column-name-here-for-hb-value REAL NOT NULL CHECK (hb > 0)
-);
